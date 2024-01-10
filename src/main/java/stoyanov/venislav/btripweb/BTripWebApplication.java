03666204f37c37dfc30f4463ff348f7a0704141e
Valsinev <streamline_@abv.bg>
2024-01-10 21:51:31 +0200

    added b-trip-lib.jar to the project (backend logic)

---
+++ src/main/java/stoyanov/venislav/btripweb/BTripWebApplication.java
@@ -0,0 +1,13 @@
+package stoyanov.venislav.btripweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTripWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTripWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stoyanov/venislav/btripweb/BTripWebApplicationTests.java
@@ -0,0 +1,13 @@
+package stoyanov.venislav.btripweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BTripWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
